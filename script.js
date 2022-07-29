824ab988ef6c9deed78e574b13bea3aebc707a48
MacBookPro2020 <boriskaborisenko@gmail.com>
2022-07-29 22:00:18 +0300

    c

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const getWorks = () => {
         const s = {style:'', font:''}
         w.position == 'Soldier' ? s.style=`padding:40px 0 40px 0px; border-radius:10px; background: linear-gradient(144deg, rgba(86,58,180,1) 0%, rgba(29,119,253,1) 20%, rgba(252,230,69,1) 70%);` : ''
         w.position == 'Soldier' ? s.font = `color:white;` : ''
+        
           
         let html = `<div class="w_s"  style="${s.style}">`
         for (let [key, value] of Object.entries(w)) {
