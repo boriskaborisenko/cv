3e2744a93b9450b0df66ae80940535597c665a6d
MacBookPro2020 <boriskaborisenko@gmail.com>
2022-07-29 21:59:31 +0300

    cv_repo

---
+++ works.js
@@ -0,0 +1,51 @@
+export const works = [
+    {
+        "dates":"March 2022 — Now",
+        "position":"Soldier",
+        "responsibilities":"Protect Ukraine against Russian aggression",
+        "employer":"Ukrainian Army Forces",
+        "type of business":"War"
+    },
+    {
+        "dates":"January 2019 — 24th February 2022",
+        "position":"Full Stack Dev/Co-Founder",
+        "responsibilities":"From scratch all. Configuring internal VWs network, setup and configuring RDBS Servers, Web Servers, API Dev. Nodejs, Express, Nginx, PHP, Front-End and a lot, lot, lot... Without Design",
+        "employer":"<a href='https://viapp.me'>viapp.me</a> (Ukraine)",
+        "type of business":"E-commerce/InsureTech/FinTech"
+    },
+    {
+        "dates":"November 2017 — December 2018",
+        "position":"Full Stack Dev",
+        "responsibilities":"From scratch all. Including Back-End, Front-End, UI/UX, Design",
+        "employer":"<a href='https://gifamin.com'>Gifamin.com</a> (Ukraine)",
+        "type of business":"E-commerce"
+    },
+    {
+        "dates":"September 2016 — July 2017",
+        "position":"Full Stack Dev",
+        "responsibilities":"From scratch to launch promotional and game sites for major TV channels",
+        "employer":"TV Channel «Ukraine» (Ukraine)",
+        "type of business":"Television"
+    },
+    {
+        "dates":"September 2015 — July 2016",
+        "position":"Front-End/Full Stack",
+        "responsibilities":"Front-End Dev, API dev, AMQP (RabbitMQ)",
+        "employer":"N.KRAT Group GmbH<br>Dortmund, Germany",
+        "type of business":"Internal Blockchain Payment System"
+    },
+    {
+        "dates":"January 2015 — May 2015",
+        "position":"Front-End Developer",
+        "responsibilities":"Front-End / API development",
+        "employer":"«Happy Tom — Food Tailored for your pet»<br>San Francisco Bay Area",
+        "type of business":"E-commerce"
+    },
+    {
+        "dates":"May 2011 — November 2014",
+        "position":"Development and support/Co-founder",
+        "employer":"<a href='https://icase.ua'>iCase.ua</a> (Kyiv)",
+        "type of business":"E-commerce"
+    }
+]
+
